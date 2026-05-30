965892f57dfa1cfaec63e1c2fbf4008b7d6dd970
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2026-05-29 23:44:29 -0700

    Implemented function invoking

--- main.py
+++ main.py
@@ -4,29 +4,7 @@ from google import genai
 from google.genai import types
 import argparse
 from prompts import system_prompt
-from functions.declarations import functions
-
-def main():
-    # Get api
-    load_dotenv()
-    api_key = os.environ.get("GEM_API_KEY")
-    if (api_key==None):
-        raise RuntimeError("Bad api key")
-    client = genai.Client(api_key=api_key)
-
-    # Read user prompt
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Show verbose output")
-    args = parser.parse_args()
-
-    messages: list[types.Content] = [
-        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
-    ]
-
-    # Get response
-    generate_content(client, messages, args.verbose)
-    
+from functions.declarations import functions, call_function
 
 def generate_content(
     client: genai.Client, messages: list[types.Content], verbose: bool
@@ -53,8 +31,44 @@ def generate_content(
         print(response.text)
         return
 
+    function_results: list[types.Part] = []
     for function_call in response.function_calls:
         print(f"Calling function: {function_call.name}({function_call.args})")
+        result = call_function(function_call=function_call, verbose=verbose)
+
+        if (
+            not result.parts
+            or not result.parts[0].function_response
+            or not result.parts[0].function_response.response
+        ):
+            raise RuntimeError(f"Empty function response for {function_call.name}")
+        
+        function_results.append(result.parts[0])
+        if verbose:
+            print(f"-> {result.parts[0].function_response.response}")
+
+
+def main():
+    # Get api
+    load_dotenv()
+    api_key = os.environ.get("GEM_API_KEY")
+    if (api_key==None):
+        raise RuntimeError("Bad api key")
+    client = genai.Client(api_key=api_key)
+
+    # Read user prompt
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Show verbose output")
+    args = parser.parse_args()
+
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+
+    # Get response
+    generate_content(client, messages, args.verbose)
+
 
 if __name__ == "__main__":
     main()
